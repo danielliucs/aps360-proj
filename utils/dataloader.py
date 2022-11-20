7a1b985a65b40ae498f1799a8958a9157cbdd6e1
Daniel Liu <daniell.liu@mail.utoronto.ca>
2022-11-20 00:50:01 -0500

    utils updated to fix imbalance issue

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -19,33 +19,73 @@ def get_relevant_indicies(dataset):
         indicies.append(idx)
     return indicies
 
-def convert_imgs_to_tensor(path, transform, video_to_frames, Yawning):
+def convert_imgs_to_tensor(path, transform, video_to_frames, Yawning, testing):
+
     video_as_tensors = []
-    for name, frames in video_to_frames.items():
-        if Yawning == False:
-            #If yawning in name we want to stop and return we have everything for normal
-            if "Yawning" in name:
-                break
-        else:
-            #If normal in name and yawning is true we skip the first couple until we get to yawning
-            if "Normal" in name:
-                continue
-        list_of_imgs_to_tensors = []
-        for jpg in frames:
-            #print(f'{path}/{jpg}')
-            img = Image.open(f'{path}/{jpg}')
-            tensor = transform(img)
-            list_of_imgs_to_tensors.append(tensor)
-        stacked_frames = torch.stack(list_of_imgs_to_tensors ) #Stacking the frames on top of one another
+    if testing:
+        for name, frames in video_to_frames.items():
+            list_of_imgs_to_tensors = []
+            for jpg in frames:
+                #print(f'{path}/{jpg}')
+                img = Image.open(f'{path}/{jpg}')
+                tensor = transform(img)
+                list_of_imgs_to_tensors.append(tensor)
+            #For varying length videos   
+            if len(list_of_imgs_to_tensors) <= 32 and len(list_of_imgs_to_tensors) > 20:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 2
+            if len(list_of_imgs_to_tensors) <= 20 and len(list_of_imgs_to_tensors) > 15:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 3
+            if len(list_of_imgs_to_tensors) <= 15 and len(list_of_imgs_to_tensors) > 10:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 5
+            if len(list_of_imgs_to_tensors) <= 10 and len(list_of_imgs_to_tensors) > 5:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 7
+            if len(list_of_imgs_to_tensors) <= 5:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 15
         
-        if stacked_frames.shape[0] != len(list_of_imgs_to_tensors):
-            raise Exception("Stacked frames for videos should be same size as number of frames")
-        #I put this here to make sure we aren't infinitely looping
-        print(len(list_of_imgs_to_tensors), stacked_frames.shape, len(frames))
-        if Yawning == False:
-            video_as_tensors.append((stacked_frames, 0)) #0 for no yawning
-        else:
-            video_as_tensors.append((stacked_frames, 1)) #1 for yawning
+            stacked_frames = torch.stack(list_of_imgs_to_tensors) #Stacking the frames on top of one another
+            #I put this here to make sure we aren't infinitely looping
+            print(name, len(list_of_imgs_to_tensors), stacked_frames.shape, len(frames))
+            if Yawning == False:
+                video_as_tensors.append((stacked_frames, 0)) #0 for no yawning
+            else:
+                video_as_tensors.append((stacked_frames, 1)) #1 for yawning
+    else:
+        for name, frames in video_to_frames.items():
+            if Yawning == False:
+                #If yawning in name we want to stop and return we have everything for normal
+                if "Yawning" in name:
+                    break
+            else:
+                #If normal in name and yawning is true we skip the first couple until we get to yawning
+                if "Normal" in name:
+                    continue
+            list_of_imgs_to_tensors = []
+            for jpg in frames:
+                #print(f'{path}/{jpg}')
+                img = Image.open(f'{path}/{jpg}')
+                tensor = transform(img)
+                list_of_imgs_to_tensors.append(tensor)
+            #For varying length videos   
+            if len(list_of_imgs_to_tensors) <= 32 and len(list_of_imgs_to_tensors) > 20:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 2
+            if len(list_of_imgs_to_tensors) <= 20 and len(list_of_imgs_to_tensors) > 15:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 3
+            if len(list_of_imgs_to_tensors) <= 15 and len(list_of_imgs_to_tensors) > 10:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 5
+            if len(list_of_imgs_to_tensors) <= 10 and len(list_of_imgs_to_tensors) > 5:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 7
+            if len(list_of_imgs_to_tensors) <= 5:
+                list_of_imgs_to_tensors = list_of_imgs_to_tensors * 15
+
+            stacked_frames = torch.stack(list_of_imgs_to_tensors) #Stacking the frames on top of one another
+
+            #I put this here to make sure we aren't infinitely looping
+            print(name, len(list_of_imgs_to_tensors), stacked_frames.shape, len(frames))
+            if Yawning == False:
+                video_as_tensors.append((stacked_frames, 0)) #0 for no yawning
+            else:
+                video_as_tensors.append((stacked_frames, 1)) #1 for yawning
+    
     return video_as_tensors
 
 def get_data(batch_size, folder, video_to_frames):#, video_to_frames):
@@ -57,10 +97,9 @@ def get_data(batch_size, folder, video_to_frames):#, video_to_frames):
         [transforms.Resize((224,224)), transforms.transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
     #Load images and convert all to tensors
-    normal = convert_imgs_to_tensor(f'{folder}/Normal', transform, video_to_frames, False)
-    yawning = convert_imgs_to_tensor(f'{folder}/Yawning', transform, video_to_frames, True)
+    normal = convert_imgs_to_tensor(f'{folder}/Normal', transform, video_to_frames, False, False)
+    yawning = convert_imgs_to_tensor(f'{folder}/Yawning', transform, video_to_frames, True, False)
 
-    yawning = yawning * 3
     #Concat both datasets
     trainset = torch.utils.data.ConcatDataset([normal, yawning])
 
@@ -69,35 +108,35 @@ def get_data(batch_size, folder, video_to_frames):#, video_to_frames):
     train_split, val_split = torch.utils.data.random_split(trainset, [train_size, val_size])
 
     #To test if we split correctly
-    for i in val_split:
-        print(i)
+    # for i in val_split:
+    #     print(i)
 
-    print("We have actually split it", train_split)
+   # print("We have actually split it", train_split)
     #SAMPLER DOES NOT WORK FOR SOME REASON LOL so I split and then do this
     #https://stackoverflow.com/questions/74291946/wrong-labels-when-using-dataloader-pytorch
     train_loader = torch.utils.data.DataLoader(train_split, batch_size=batch_size, num_workers=1)
     val_loader = torch.utils.data.DataLoader(val_split, batch_size=batch_size, num_workers=1)
-    print("Our dataloader", train_loader)
+    #print("Our dataloader", train_loader)
     return train_loader, val_loader
 
-def get_testing_data(batch_size, folder, video_to_frames):
+def get_testing_data(batch_size, folder, yawning_frames, normal_frames):
     transform = transforms.Compose(
         [transforms.Resize((224,224)), transforms.transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
     #Load images and convert all to tensors
-    normal_testing = convert_imgs_to_tensor(f'{folder}/testing_normal', transform, video_to_frames, False)
-    yawning_testing = convert_imgs_to_tensor(f'{folder}/testing_yawning', transform, video_to_frames, True)
+    normal_testing = convert_imgs_to_tensor(f'{folder}/testing_normal', transform, normal_frames, False, True)
+    print("NOW FOR YAWNING!")
+    yawning_testing = convert_imgs_to_tensor(f'{folder}/testing_yawning', transform, yawning_frames, True, True)
 
-    yawning_testing = yawning_testing * 3
     #Concat both datasets
     testset = torch.utils.data.ConcatDataset([normal_testing, yawning_testing])
 
     #To test if it's alright
-    for i in testset:
-        print(i)
+    # for i in testset:
+    #     print(i)
 
     test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, num_workers=1, shuffle=True)
-    print("Our dataloader", test_loader)
+    #print("Our dataloader", test_loader)
     return test_loader
     
 def visualize(loader):
@@ -138,7 +177,6 @@ def save_features(loader, train, dir):
         torch.save(img, f'{dir}/{classes[label]}/{img_count}.tensorsaved')
         img_count += 1
 
-all_file_paths = []
 all_file_names = []
 
 def scanFiles(directory):
@@ -148,23 +186,64 @@ def scanFiles(directory):
         if filename.is_dir():
             scanFiles(filename.path)
         if filename.is_file():
-            all_file_paths.append(filename.path)
             all_file_names.append(filename.name)
             
-def map_frames_to_vid():
-    img_arr = []
-    idx = all_file_names[0].find('.')
-    vid_name = all_file_names[0][:idx]
-    video_to_frames = {}
-    for img in all_file_names:
-        curr_idx = img.find('.')
-        curr_name = img[:curr_idx]
-        if curr_name != vid_name: #The next video is here
-            video_to_frames[vid_name] = img_arr #Save current images of that video into hash table
-            img_arr = [] #Reset image array
-            vid_name = curr_name #New vid_name
-        img_arr.append(img)
-    return video_to_frames
+def map_frames_to_vid(testing, file_names):
+    if testing:
+        img_arr = []
+        idx = file_names[0].find('MOV')
+        if idx == -1:
+            idx = file_names[0].find('mov') 
+
+        if idx == -1:
+            vid_name = file_names[0]
+        else:
+            vid_name = file_names[0][:idx]
+
+        video_to_frames = {}
+        for img in file_names:
+            curr_idx = img.find('MOV')
+            if curr_idx == -1:
+                curr_idx = img.find('mov')
+
+            if curr_idx == -1:
+                curr_name = img
+            else:
+                curr_name = img[:curr_idx]
+
+            if curr_name != vid_name: #The next video is here
+                video_to_frames[vid_name] = img_arr #Save current images of that video into hash table
+                img_arr = [] #Reset image array
+                vid_name = curr_name #New vid_name
+            img_arr.append(img)
+        return video_to_frames
+    else:
+        img_arr = []
+        idx = all_file_names[0].find('.')
+        vid_name = all_file_names[0][:idx]
+        video_to_frames = {}
+        for img in all_file_names:
+            curr_idx = img.find('.')
+            curr_name = img[:curr_idx]
+            if curr_name != vid_name: #The next video is here
+                video_to_frames[vid_name] = img_arr #Save current images of that video into hash table
+                img_arr = [] #Reset image array
+                vid_name = curr_name #New vid_name
+            img_arr.append(img)
+        return video_to_frames
+
+def get_yawn_or_normal_files(list, yawning):
+
+    for name in all_file_names:
+        if yawning:
+            idx = name.find("Yawning")
+        else:
+            idx = name.find("Normal")
+        if idx != -1:
+            list.append(name)
+
+    return list
+            
 
 def main():
     
@@ -172,13 +251,23 @@ def main():
     #Change the paths to your own paths
     #TODO maybe make it through user input
     #TODO wrap things into a class for more organization maybe
-    scanFiles("C:/Users/User/Desktop/aps360-proj/dataset")
-    video_to_frames = map_frames_to_vid()
-    #get_data(1, "C:/Users/User/Desktop/aps360-proj/dataset")
-    #print(video_to_frames)
-    train_loader, data_loader = get_data(1, "C:/Users/User/Desktop/aps360-proj/dataset", video_to_frames)
-    save_features(train_loader, "train", "C:/Users/User/Desktop/aps360-proj/dataset")
-    save_features(data_loader, "val", "C:/Users/User/Desktop/aps360-proj/dataset")
+    scanFiles("C:/Users/User/Desktop/aps360-proj/testing_dataset")
+    yawn_files = []
+    normal_files = []
+    get_yawn_or_normal_files(yawn_files, True)
+    get_yawn_or_normal_files(normal_files, False)
+    yawn_set = map_frames_to_vid(True, yawn_files)
+    normal_set = map_frames_to_vid(True, normal_files)
+    #print(normal_set)
+    # video_to_frames = map_frames_to_vid(False, [])
+    # get_data(1, "C:/Users/User/Desktop/aps360-proj/dataset", video_to_frames)
+    # #print(video_to_frames)
+    # train_loader, data_loader = get_data(1, "C:/Users/User/Desktop/aps360-proj/dataset", video_to_frames)
+    # save_features(train_loader, "train", "C:/Users/User/Desktop/aps360-proj/dataset")
+    # save_features(data_loader, "val", "C:/Users/User/Desktop/aps360-proj/dataset")
+    
+    test_loader = get_testing_data(1, "C:/Users/User/Desktop/aps360-proj/testing_dataset", yawn_set, normal_set)
+    save_features(test_loader, "test", "C:/Users/User/Desktop/aps360-proj/testing_dataset")
     
 
 if __name__ == '__main__':
