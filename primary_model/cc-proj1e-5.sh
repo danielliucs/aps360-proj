55f5326fa11a375f46d52f10218cc7efb3be3198
Daniel Liu <daniell.liu@mail.utoronto.ca>
2022-11-20 01:39:37 -0500

    compute canada scripts added

---
+++ primary_model/cc-proj1e-5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --nodes=3
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128G
+#SBATCH --account=def-baochun
+#SBATCH --output=results5.out
+
+module load python
+source ~/.proj/bin/activate
+python main5.py 
